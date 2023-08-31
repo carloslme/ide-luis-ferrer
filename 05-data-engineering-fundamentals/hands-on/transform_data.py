ab408088f8489d3429bcf5ecc36b4bdb9d1c585a
carloslme <32799711+carloslme@users.noreply.github.com>
2023-08-30 22:03:53 -0600

    feat: refactor etl pipeline

--- 05-data-engineering-fundamentals/hands-on/transform_data.py
+++ 05-data-engineering-fundamentals/hands-on/transform_data.py
@@ -3,21 +3,41 @@ import os
 import pandas as pd
 
 # Extraction
-df = pd.read_csv('05-data-engineering-fundamentals/hands-on/data/Absenteeism_at_work.csv', sep=';')
+def extract_data(path: str):
+    df = pd.read_csv(path, sep=';')
+    print("Data Extracted")
+    return df
 
 # Transformation
-def rename_columns(x):
-    if " " in x:
-        return x.replace(' ', '_').lower() 
-    return x.lower()
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    """This function transform the input data.
+
+    Args:
+        df (pd.DataFrame): Raw dataframe
+    Returns:
+        pd.DataFrame: Transformed dataframe
+    """
+    def rename_columns(x):
+        if " " in x:
+            return x.replace(' ', '_').replace('/', '_').lower() 
+        return x.lower()
+    df = df.rename(columns=rename_columns)
+    print("Data Transformed")
+    return df
 
 # Load
-df = df.rename(columns=rename_columns)
-df.to_csv("05-data-engineering-fundamentals/hands-on/data/Absenteeism_at_work_transformed.csv", encoding='utf-8')
+def load_data(path: str, df: pd.DataFrame):
+    df.to_csv(path, encoding='utf-8')
+    print("Data Loaded")
 
-def run_etl():
-    pass
+def run_etl(raw_data_path: str, output_data_pat: str):
+    raw_df = extract_data(raw_data_path)
+    transformed_df = transform_data(raw_df)
+    load_data(output_data_pat, transformed_df)
+    
 
 if __name__ == "__main__":
     print(os.getcwd())
-    run_etl()
+    raw_data_path = '05-data-engineering-fundamentals/hands-on/data/Absenteeism_at_work.csv'
+    output_data_path = "05-data-engineering-fundamentals/hands-on/data/staging/Absenteeism_at_work_transformed.csv"
+    run_etl(raw_data_path, output_data_path)
